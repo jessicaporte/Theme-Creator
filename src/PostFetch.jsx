f8b5ffe00702f9eea4dc63d6f0bebd11a0bd0f1d
jessica <jessy_marian91@hotmail.com>
2025-04-04 14:30:20 +0200

    ok

---
+++ src/PostFetch.jsx
@@ -0,0 +1,21 @@
+/*async function postFetch(hex,contrastText) {
+    const response = await fetch(
+      "https://www.aremycolorsaccessible.com/api-page",
+      
+      {
+        method: "POST",
+        body: JSON.stringify({ backgrounColor : hex 
+            textColor : contrastText
+         }),
+        headers: {
+         "Content-Type": "application/json",
+       },
+      }
+    );
+    const data = await response.json(); 
+}
+if(!response.ok){console.log("error");
+    return;
+}
+
+if */
